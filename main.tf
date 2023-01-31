cbaae90c959e07c969d255ec88d1cc4b811e3342
newhopemusician <95931293+newhopemusician@users.noreply.github.com>
2023-01-31 12:14:24 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-	ami = "ami-0cbbb4eeb4641a90d"
+	ami = "ami-061e388c127cfdae7"
 	subnet_id = aws_subnet.publicsubnet.id
 	instance_type = "t2.micro"
   	vpc_security_group_ids = [aws_security_group.allow_all.id]
